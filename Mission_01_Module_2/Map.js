23e453eef8b5e5bf375fff1d5349c2e097541629
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-02 10:22:04 +0600

    map define not complete yet

---
+++ Mission_01_Module_2/Map.js
@@ -0,0 +1,7 @@
+const obj = {
+  nextLevel: { courseId: "Level 1" },
+  "Programming Hero": { courseId: "Level 2" },
+};
+console.log(obj.nextLevel); // or we can access like this
+console.log(obj["nextLevel"]); // or we can access like this
+console.log(obj["Programming Hero"]);
