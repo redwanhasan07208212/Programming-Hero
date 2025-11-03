cc194ad5520652e97df11be017aaf6eee637b2dd
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-03 23:58:55 +0600

    change something

--- Mission_01_Module_2/Map.js
+++ Mission_01_Module_2/Map.js
@@ -5,3 +5,10 @@ const obj = {
 console.log(obj.nextLevel); // or we can access like this
 console.log(obj["nextLevel"]); // or we can access like this
 console.log(obj["Programming Hero"]);
+
+const obj2 = {};
+const course1 = { name: "JavaScript", duration: "3 months" };
+const course2 = { name: "Python", duration: "4 months" };
+
+obj2["course1"] = { courseId: "JS-101", ...course1 };
+console.log(obj2);
