eb33a67607284a803538b6b9efb01fed77f9f85b
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-07 04:56:10 +0600

    reduce method practice

--- Mission_01_Module_2/Reduce.js
+++ Mission_01_Module_2/Reduce.js
@@ -32,3 +32,39 @@ const bestScorer = players.reduce((bestPlayer, player) => {
 }, players[0]);
 
 console.log(bestScorer);
+
+//?  sum array
+const numbers = [5, 10, 15, 20];
+
+console.log(numbers.reduce((acc, sum) => acc + sum, 0));
+
+//?  max number in array
+const nums = [10, 25, 3, 47, 5];
+
+const maxNumber = nums.reduce((max, num) => {
+  if (max > num) {
+    return max;
+  }
+  return num;
+}, nums[0]);
+console.log(maxNumber);
+
+//? Count total characters
+//! 👉 Find total number of characters in all words combined
+//! Output: 16
+const words = ["hello", "world", "reduce"];
+
+const countTotalChars = words.reduce((total, word) => {
+  return total + word.length;
+}, 0);
+console.log(countTotalChars);
+
+//! Count occurrences
+
+const fruits = ["apple", "banana", "apple", "orange", "banana", "apple"];
+const fruitCount = fruits.reduce((count, fruit) => {
+  count[fruit] = (count[fruit] || 0) + 1;
+  return count;
+}, {});
+
+console.log(fruitCount);
