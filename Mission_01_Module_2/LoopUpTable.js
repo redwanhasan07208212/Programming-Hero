992e459c35f1fa59bcef36751004c94826ecfeb3
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-07 13:00:22 +0600

    look up table

---
+++ Mission_01_Module_2/LoopUpTable.js
@@ -0,0 +1,18 @@
+const postsArray = [
+  { id: "p-101", title: "Intro to SQL", author: "Alex" },
+  { id: "p-102", title: "Data Structures in JS", author: "Beth" },
+  { id: "p-103", title: "Understanding Reduce", author: "Chris" },
+  { id: "p-104", title: "CSS Grid Tricks", author: "Alex" },
+];
+
+const lookUpTable = postsArray.reduce((table, post) => {
+  table[post.id] = post;
+
+  return table;
+}, {});
+
+// console.log(lookUpTable);
+
+for (let post in lookUpTable) {
+  console.log(post, lookUpTable[post]);
+}
