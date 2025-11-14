36ee2d1933c9d25b46539b9653d2c15d6bb96e0a
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-14 19:04:13 +0600

    append done

--- Mission_01_Module_3/MO3VO6.js
+++ Mission_01_Module_3/MO3VO6.js
@@ -4,3 +4,39 @@ class Node {
     this.next = null;
   }
 }
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  print() {
+    const arr = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(arr.join(" -> "), "-> null ");
+  }
+}
+
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+linkedList.print();
