37420b1c7c192934f076dccdcfbf3ebe93a7e87a
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-21 00:32:07 +0600

    update

---
+++ Mission_02_Module_5/src/1.4.js
@@ -0,0 +1,27 @@
+{
+    var FirstName = "Redwan Hasan";
+    var digit = 34;
+    var isAdmin = false;
+    var x = null;
+    var courses = [
+        "Web Development",
+        "Data Science",
+        "Machine Learning",
+    ];
+    var eligibleRoll = [34, 78, 65, 80];
+    var d = // this is called the anytype data structure its not good practice for declare anytype.
+     void 0; // this is called the anytype data structure its not good practice for declare anytype.
+    d = 8;
+    d = "Hello";
+    d = [1, 2, 3, 4]; // thats why we are not using the anytype data structure because we can assign different value in one variable.
+    var coordinates = [34, 67];
+    var ageName = [45, "Redwan"]; // this is called tuple
+    console.log("First Name:", FirstName);
+    console.log("Digit:", digit);
+    console.log("Is Admin:", isAdmin);
+    console.log("Courses:", courses);
+    console.log("Eligible Roll Numbers:", eligibleRoll);
+    console.log("x:", x);
+    console.log("Coordinates:", coordinates);
+    console.log("Age and Name:", ageName);
+}
