f9669ab5c2b63fbed9e55a0d55926fea416bf07e
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-21 01:45:21 +0600

    primitive type

---
+++ Mission_02_Module05/dist/primitive.js
@@ -0,0 +1,11 @@
+"use strict";
+// JS - TS
+// string, number, boolean, undefined, null
+Object.defineProperty(exports, "__esModule", { value: true });
+// TS: never, unknown, void
+let userName = "Redwan";
+userName = "Hasan";
+let userId;
+userId = 123.4533;
+userId.toFixed(3);
+//# sourceMappingURL=primitive.js.map
---
+++ Mission_02_Module05/src/primitive.ts
@@ -0,0 +1,14 @@
+// JS - TS
+// string, number, boolean, undefined, null
+
+// TS: never, unknown, void
+
+let userName: string = "Redwan"; // lowercase 'string'
+userName = "Hasan";
+let userId: number; // lowercase 'number'
+userId = 123.4533;
+let formattedIda: String = userId.toFixed(3);
+let isAdmin: boolean = false; // lowercase 'boolean'
+console.log(formattedIda);
+
+let x: undefined = undefined;
