c77e4a1aa9d9c5e2f2ea9ddfc0be5b28df457709
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-22 14:00:55 +0600

    typescript object literal type

--- Mission_02_Module05/src/nonPrimitive.ts
+++ Mission_02_Module05/src/nonPrimitive.ts
@@ -1,3 +1,7 @@
+/**********************************************
+ // ??? Reference Type: Array 
+ // ??? Tuple
+ **********************************************/
 let bazarList: string[] = ["Tomato", "Chillies", "Orange"];
 
 let mixedArr: (string | number)[] = ["Redwan", 1, "Hasan", 2];
@@ -10,3 +14,34 @@ const destination: [string, string, string] = [
   "Chittagong",
   "Rajshahi",
 ];
+
+/**********************************************
+ // ??? Reference Type: Object
+ // ??? Object
+ **********************************************/
+
+const user: {
+  organizationType: "BatMan"; // ?This is object literal that we can not change the value
+  firstName: string;
+  middleName?: string; /// OPTIONAL TYPE
+  lastName: string;
+} = {
+  organizationType: "BatMan",
+  firstName: "Redwan",
+  lastName: "Hasan",
+};
+
+const user2: {
+  readonly organizationType: string;
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+} = {
+  organizationType: "BATMAN WIFE",
+  firstName: "Effat",
+  middleName: "Ara",
+  lastName: "Supti",
+};
+
+console.log(user);
+console.log(user2);
