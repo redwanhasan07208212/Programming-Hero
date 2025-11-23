410edf9dad49a61a417e737b9ae47f1ab4f7704f
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-23 22:59:02 +0600

    union and intersection

---
+++ Mission_02_Module05/src/union&intersection.ts
@@ -0,0 +1,41 @@
+/**********************************************
+ // ??? Type: Union
+**********************************************/
+
+type userRole = "admin" | "user" | "guest";
+
+const getDashboard = (user: userRole) => {
+  if (user === "admin") {
+    return "admin dashboard";
+  } else if (user === "user") {
+    return "user dashboard";
+  } else {
+    return "guest dashboard";
+  }
+};
+
+console.log(getDashboard("guest"));
+
+/**********************************************
+ // ??? Type: Intersection 
+**********************************************/
+type Employee = {
+  id: string;
+  name: string;
+  phoneNo: string;
+};
+
+type Manager = {
+  designation: string;
+  teamSize: number;
+};
+
+type EmployeeManager = Employee & Manager;
+
+const ChowdhuryShaheb: EmployeeManager = {
+  id: "123",
+  name: "Chowdhury Shaheb",
+  phoneNo: "017",
+  designation: "manager",
+  teamSize: 20,
+};
