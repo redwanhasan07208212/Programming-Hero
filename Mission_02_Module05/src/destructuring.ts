315bd12fb949de50806e64852e12dbcd00db9bd4
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-23 12:14:10 +0600

    Array Destructuring

---
+++ Mission_02_Module05/src/destructuring.ts
@@ -0,0 +1,29 @@
+/**********************************************
+ // ??? Type: Object Destructuring
+**********************************************/
+
+const user = {
+  id: 123,
+  name: {
+    firstName: "Redwan",
+    middleName: "Hasan",
+    lastName: "Siam",
+  },
+  gender: "male",
+  favoriteColor: "white",
+};
+
+const {
+  favoriteColor,
+  name: { firstName, middleName },
+} = user;
+
+console.log(firstName, middleName, favoriteColor);
+
+/**********************************************
+ // ??? Type: Array Destructuring
+**********************************************/
+
+const ara = ["Chintu", "Bablu", "Shiblu", "Montu", "Shontu"];
+const [A, B, C] = ara;
+console.log(A, B, C);
