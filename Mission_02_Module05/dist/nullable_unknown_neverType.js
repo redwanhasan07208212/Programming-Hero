172db89d9a7763983711c82fa73120d817fa7d08
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-25 16:16:32 +0600

    module 5 complete

--- Mission_02_Module05/dist/nullable_unknown_neverType.js
+++ Mission_02_Module05/dist/nullable_unknown_neverType.js
@@ -1,3 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// nullable type
+const getUser = (input) => {
+    if (input) {
+        console.log(`FROM DB ${input}`);
+    }
+    else {
+        console.log(`FROM DB ALL USER`);
+    }
+};
+getUser(null);
+// unknown type
+const discountedCalculator = (input) => {
+    if (typeof input === "number") {
+        const discountedPrice = input * 0.1;
+        console.log(discountedPrice);
+    }
+    else if (typeof input === "string") {
+        const [discountedPrice] = input.split(" ");
+        console.log(Number(discountedPrice) * 0.1);
+    }
+    else {
+        console.log("wrong input");
+    }
+};
+discountedCalculator(100);
+discountedCalculator("100 tk");
+discountedCalculator(null);
+// void
+const throwError = (msg) => {
+    throw new Error(msg);
+};
+throwError("Error...");
 //# sourceMappingURL=nullable_unknown_neverType.js.map
