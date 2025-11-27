8016a8badd66f61eb2bbbb38b8d7dce00a099e14
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-11-27 23:29:19 +0600

    update

---
+++ Mission_02_Module06/dist/interface.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const user1 = {
+    name: "Mr.X",
+    age: 100,
+    role: "admin",
+};
+const user2 = {
+    name: "Mr. Y",
+    age: 102,
+};
+const isAdmin = false;
+const add = (num1, num2) => num1 + num2;
+const friends = ["A", "B", "C"];
+//# sourceMappingURL=interface.js.map
