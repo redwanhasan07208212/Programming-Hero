1380a70bb55fb59d32196ece5e6ed50b10c3becd
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-02 22:48:12 +0600

    type guard

---
+++ Mission_02_Module_7/src/typeGuard.ts
@@ -0,0 +1,42 @@
+// type guard
+
+// in typeof
+
+type Alphanumeric = number | string;
+
+const add = (num1: Alphanumeric, num2: Alphanumeric) => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else {
+    num1.toString() + num2.toString();
+  }
+};
+
+add(2, 2); // 4
+
+add(2, "2"); // 22
+
+add("2", 2); // 22
+
+console.log(add("2", "2")); //
+
+// in guard
+
+type NormalUser = {
+  name: string;
+};
+
+type AdminUser = {
+  name: string;
+  role: "Admin";
+};
+
+const getUserInfo = (user: NormalUser | AdminUser) => {
+  if ("role" in user) {
+    console.log(`${user.name} and his role is : ${user.role}`);
+  } else {
+    console.log(`${user.name} is a normal user`);
+  }
+};
+
+getUserInfo({ name: "Redwan", role: "Admin" });
