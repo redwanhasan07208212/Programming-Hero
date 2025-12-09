9e9339d40fcaf0c8d5f4c87c657598bee46d8aeb
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-10 01:38:38 +0600

    individual product route done

--- ConceptualSession/NodeJsConceptual/src/controller/product.controller.ts
+++ ConceptualSession/NodeJsConceptual/src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProductsFromDB } from "../services/product.service";
+import type { IProduct } from "../types/product.interface";
 
 export const productController = (
   req: IncomingMessage,
@@ -7,9 +8,27 @@ export const productController = (
 ) => {
   const method = req.method;
   const url = req.url;
+  const urlPart = url?.split("/");
+
+  const id = urlPart && urlPart[1] === "products" ? Number(urlPart[2]) : null;
 
   if (method === "GET" && url === "/products") {
     res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify({ products: readProductsFromDB() }));
+    res.end(
+      JSON.stringify({
+        message: "This is Product Route",
+        products: readProductsFromDB(),
+      })
+    );
+  } else if (method === "GET" && id !== null) {
+    const products = readProductsFromDB();
+    const product = products.find((p: IProduct) => p.id === id);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "This is individual product route",
+        data: product,
+      })
+    );
   }
 };
