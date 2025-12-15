b4b93ab6dde6e1b2e3c38da98072bf97db0b93b0
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-15 23:18:18 +0600

    server

--- Mission_03_Module_12/src/server.ts
+++ Mission_03_Module_12/src/server.ts
@@ -8,3 +8,12 @@ const server: Server = createServer((req, res) => {
 server.listen(8000, () => {
   console.log("Server is running on http://localhost:8000");
 });
+// logger middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+  next();
+};
+
+app.get("/", logger, (req: Request, res: Response) => {
+  res.send("Hello Next Level Developers!");
+});
