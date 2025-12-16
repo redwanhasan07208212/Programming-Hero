ee08e30bd6825665f92b39577932f5d41875b1c2
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-16 13:06:36 +0600

    initialize post gres

--- Mission_03_Module_12/src/server.ts
+++ Mission_03_Module_12/src/server.ts
@@ -1,11 +1,25 @@
 import express, { Request, Response } from "express";
+import { Pool } from "pg";
 const app = express();
 const port = 8000;
+//parser
 
+app.use(express.json());
+app.use(express.urlencoded());
+
+const pool = new Pool({
+  connectionString: `postgresql://neondb_owner:npg_5bWFqou6hcjO@ep-rough-darkness-a1rqae7x-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+});
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Next Level Developers!");
 });
-
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(201).json({
+    success: true,
+    message: "Api Working",
+  });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
