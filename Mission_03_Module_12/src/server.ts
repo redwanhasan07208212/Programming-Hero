65a72c946a1a4f2c28d5e26c7d40f56da1468d18
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-16 12:33:03 +0600

    express define

--- Mission_03_Module_12/src/server.ts
+++ Mission_03_Module_12/src/server.ts
@@ -1,19 +1,21 @@
-import { createServer, Server } from "http";
-import { productRoute } from "./routes/product.route";
+import express, { Request, Response } from "express";
+const app = express();
+const port = 8000;
 
-const server: Server = createServer((req, res) => {
-  productRoute(req, res);
-});
-
-server.listen(8000, () => {
-  console.log("Server is running on http://localhost:8000");
-});
-// logger middleware
-const logger = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
-  next();
-};
-
-app.get("/", logger, (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello Next Level Developers!");
 });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// logger middleware
+// const logger = (req: Request, res: Response, next: NextFunction) => {
+//   console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+//   next();
+// };
+
+// app.get("/", logger, (req: Request, res: Response) => {
+//   res.send("Hello Next Level Developers!");
+// });
