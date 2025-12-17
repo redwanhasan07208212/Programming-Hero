2baffe8e30921b70748c4846142c21a1853deff8
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-17 16:04:08 +0600

    clean the file

--- Mission_03_Module_12/src/server.ts
+++ Mission_03_Module_12/src/server.ts
@@ -1,8 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
-import { Pool, Result } from "pg";
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+import initDB, { pool } from "./config/db";
+
 const app = express();
 const port = 8000;
 
@@ -11,36 +9,6 @@ const port = 8000;
 app.use(express.json());
 app.use(express.urlencoded());
 
-const pool = new Pool({
-  connectionString: `${process.env.CONNECTION_STR}`,
-});
-const initDB = async () => {
-  await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )
-        `);
-
-  await pool.query(`
-            CREATE TABLE IF NOT EXISTS todos(
-            id SERIAL PRIMARY KEY,
-            user_id INT REFERENCES users(id) ON DELETE CASCADE,
-            title VARCHAR(200) NOT NULL,
-            description TEXT,
-            completed BOOLEAN DEFAULT false,
-            due_date DATE,
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-            )
-            `);
-};
 initDB();
 
 //logger middleware
