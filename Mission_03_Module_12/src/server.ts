c9df9ded7c5de263113733987340e3477127ebb6
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-17 12:57:26 +0600

    env added and new table added

--- Mission_03_Module_12/src/server.ts
+++ Mission_03_Module_12/src/server.ts
@@ -1,15 +1,44 @@
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 const app = express();
 const port = 8000;
+
 //parser
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 const pool = new Pool({
-  connectionString: `postgresql://neondb_owner:npg_5bWFqou6hcjO@ep-rough-darkness-a1rqae7x-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+  connectionString: `${process.env.CONNECTION_STR}`,
 });
+const initDB = async () => {
+  await pool.query(`CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    age INT,
+    phone VARCHAR(15),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )`);
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS todos(
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id) ON DELETE CASCADE,
+        title VARCHAR NOT NULL,
+        description TEXT,
+        completed BOOLEAN DEFAULT false,
+        due_date DATE,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+};
+initDB();
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Next Level Developers!");
 });
