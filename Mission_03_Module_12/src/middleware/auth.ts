9b941d8ee4b1700bb670ed3e35637b101960090e
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-22 16:12:42 +0600

    express type declare

--- Mission_03_Module_12/src/middleware/auth.ts
+++ Mission_03_Module_12/src/middleware/auth.ts
@@ -1,18 +1,26 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 import config from "../config";
 
 const auth = () => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
-    if (!token) {
-      return res.status(500).json({
-        message: "You are not allowed to access this route!!",
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        return res.status(500).json({
+          message: "You are not allowed to access this route!!",
+        });
+      }
+      const decoded = jwt.verify(token, config.secret_key as string);
+      console.log({ decoded });
+      req.user = decoded as JwtPayload;
+      next();
+    } catch (err: any) {
+      res.status(500).json({
+        status: false,
+        message: err.message,
       });
     }
-    const decoded = jwt.verify(token, config.secret_key as string);
-    console.log({ decoded });
-    next();
   };
 };
 export default auth;
