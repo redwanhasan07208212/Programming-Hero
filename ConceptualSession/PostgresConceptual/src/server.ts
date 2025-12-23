9c40119ce449caac3d5b4df680fefddad1f2a54f
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-23 23:09:56 +0600

    postgre initialized

--- ConceptualSession/PostgresConceptual/src/server.ts
+++ ConceptualSession/PostgresConceptual/src/server.ts
@@ -1,5 +1,13 @@
+import { Pool } from "pg";
 import express, { Request, Response } from "express";
 const app = express();
+
+app.use(express.json());
+
+const pool = new Pool();
+app.post("/users", (req: Request, res: Response) => {
+  console.log(req.body);
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
