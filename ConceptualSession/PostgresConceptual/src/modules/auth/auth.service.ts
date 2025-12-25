641281b97347b9108ed5afdab9d12233dd611ba5
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-25 12:32:27 +0600

    password logic done

--- ConceptualSession/PostgresConceptual/src/modules/auth/auth.service.ts
+++ ConceptualSession/PostgresConceptual/src/modules/auth/auth.service.ts
@@ -1,4 +1,15 @@
-const loginUserIntoDb = async (email: string, password: string) => {};
+import bcrypt from "bcryptjs";
+import { pool } from "../../config/db";
+
+const loginUserIntoDb = async (email: string, password: string) => {
+  const user = await pool.query(`SELECT * FROM users WHERE email=$1`, [email]);
+
+  const userPassword = user.rows[0].password;
+  const matchPassword = bcrypt.compare(password, userPassword);
+  if (!matchPassword) {
+    throw new Error("Invalid Credentials");
+  }
+};
 
 export const authService = {
   loginUserIntoDb,
